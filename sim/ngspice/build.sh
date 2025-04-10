2804ca4dcfeec2f39b8c5e480c30a97f3a1f37cc
Duy-Hieu Bui <duyhieu.bui@gmail.com>
2025-04-10 18:20:40 +0700

    Disable readline and enable osdi for ngspice to support IHP Open PDK

--- sim/ngspice/build.sh
+++ sim/ngspice/build.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 ./autogen.sh
-./configure --prefix="${PREFIX}" --disable-debug
+./configure --prefix="${PREFIX}" --disable-debug --with-readline=no --enable-osdi
 make V=1 -j$CPU_COUNT
 make V=1 install
