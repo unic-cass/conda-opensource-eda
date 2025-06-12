5931765d1ef59fa453e2a1a47db34beadfafbc29
Duy-Hieu Bui <duyhieu.bui@gmail.com>
2025-06-12 07:03:58 +0700

    Enable TCL for yosys

--- syn/yosys/build.sh
+++ syn/yosys/build.sh
@@ -15,13 +15,13 @@ case "${UNAME_OUT}" in
                 exit;;
 esac
 echo "Build started for ${OS}..."
-echo "PREFIX := $PREFIX" >> Makefile.conf
+echo "PREFIX := $PREFIX" > Makefile.conf
 echo "ENABLE_READLINE := 0" >> Makefile.conf
-echo "ENABLE_TCL := 0" >> Makefile.conf
+# echo "ENABLE_TCL := 0" >> Makefile.conf
 
 if [[ $OS == "Linux" ]]; then
-    ln -s $GCC $BUILD_PREFIX/bin/gcc
-    make config=gcc-static V=1 -j$CPU_COUNT
+    ln -sf $GCC $BUILD_PREFIX/bin/gcc
+    make CONFIG=gcc V=1 -j$CPU_COUNT
 elif [[ $OS == "Mac" ]]; then
     make config-mac
 fi
