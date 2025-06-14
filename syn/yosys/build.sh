0752e79419ae81bc2e3dbfb98b3d023f01915656
Duy-Hieu Bui <duyhieu.bui@gmail.com>
2025-06-14 15:18:43 +0700

    Fix build scripts for yosys

--- syn/yosys/build.sh
+++ syn/yosys/build.sh
@@ -17,10 +17,13 @@ esac
 echo "Build started for ${OS}..."
 echo "PREFIX := $PREFIX" > Makefile.conf
 echo "ENABLE_READLINE := 0" >> Makefile.conf
-# echo "ENABLE_TCL := 0" >> Makefile.conf
+echo "CONFIG := gcc" >> Makefile.conf
+echo "ENABLE_TCL := 1" >> Makefile.conf
+echo "ENABLE_PLUGINS := 1" >> Makefile.conf
 
 if [[ $OS == "Linux" ]]; then
     ln -sf $GCC $BUILD_PREFIX/bin/gcc
+    ln -sf $CXX $BUILD_PREFIX/bin/g++
     make CONFIG=gcc V=1 -j$CPU_COUNT
 elif [[ $OS == "Mac" ]]; then
     make config-mac
@@ -30,5 +33,6 @@ fi
 # make V=1 -j$CPU_COUNT
 make install
 
+export LD_LIBRARY_PATH=$PREFIX/lib
 $PREFIX/bin/yosys -V
 $PREFIX/bin/yosys-abc -v 2>&1 | grep compiled
