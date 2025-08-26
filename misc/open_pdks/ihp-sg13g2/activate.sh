a1f18f20e5edf2a9d56ad467759ef283f636ccd4
Duy-Hieu Bui <duyhieu.bui@gmail.com>
2025-08-27 06:08:39 +0700

    Modify activate script for ihp-sg13g2

--- misc/open_pdks/ihp-sg13g2/activate.sh
+++ misc/open_pdks/ihp-sg13g2/activate.sh
@@ -1,3 +1,9 @@
-export PDK_ROOT=$CONDA_PREFIX/share/pdk
-export PDK=ihp-sg13g2
+_PDK_=ihp-sg13g2
+_PDK_ROOT_=$CONDA_PREFIX/share/pdk
+[[ ! -z "${PDK_ROOT}" ]] && echo "PDK_ROOT is defined as $PDK_ROOT. Overwrite it to $_PDK_ROOT_"
+export PDK_ROOT=$_PDK_ROOT_
+
+[[ ! -z "${PDK}" ]] && echo "PDK is defined as $PDK. Overwrite it to $_PDK_"
+export PDK=$_PDK_
+[[ ! -z "${SPICE_SCRIPTS}" ]] && echo "SPICE_SCRIPT is defined as $SPICE_SCRIPTS. Overwrite it to $PDK_ROOT/$PDK/libs.tech/ngspice"
 export SPICE_SCRIPTS=$PDK_ROOT/$PDK/libs.tech/ngspice
