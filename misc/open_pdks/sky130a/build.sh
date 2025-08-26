6eae1d2d6e715374acaad8c9995c9af0e41a6135
Duy-Hieu Bui <duyhieu.bui@gmail.com>
2025-08-27 06:06:19 +0700

    Add activeate/deactivate script for sky130A

--- misc/open_pdks/sky130a/build.sh
+++ misc/open_pdks/sky130a/build.sh
@@ -60,3 +60,11 @@ VARIANT=${VARIANT^^}
   --with-sky130-variants=$VARIANT
 make V=1
 make V=1 install
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
